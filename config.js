45bf74d9a2dca2dc22b78b6bbde86192aacab1e1
Dog BarK <137015173+D0gBarK@users.noreply.github.com>
2023-06-29 21:26:28 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://michealj.liamwhite078.workers.dev",
+  VITE_TMDB_API_KEY: "6bef9c2d0d98b9166bed099432cefd17",
+  VITE_OMDB_API_KEY: "478edbb0",
+};
